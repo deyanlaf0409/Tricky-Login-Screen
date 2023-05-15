c56b5c3c80487a448303b20f4c0449177bf18343
deyanlaf0409 <dlafchiev@gmail.com>
2023-05-15 12:10:41 +0300

    Separate css code and added validation logic

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
 var myButton = document.getElementById("evil-button");
-var xPos = 660;
+var xPos = myButton.offsetLeft; // Get the initial left position of the button
 var isLeft = true;
 
 myButton.onmouseover = function(e) {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
   if (!username || !password) {
     if (isLeft) {
-      xPos = 575;
+      xPos = 260;
     } else {
-      xPos = 755;
+      xPos = 80;
     }
     myButton.style.left = xPos + "px";
     isLeft = !isLeft;
   }
+};
+
+function checkLogin(event) {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  if (!username || !password) {
+    alert("Please enter both username and password.");
+    event.preventDefault(); // Cancel the form submission
+    return false;
+  }
+  // Other validation logic...
+
+  // If the validation passes, reload the page
+  window.location.reload();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
